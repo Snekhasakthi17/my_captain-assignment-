38f5c0e40c84e4d019b668d7fbae85fc1a969f0f
Snekhasakthi17 <103012502+Snekhasakthi17@users.noreply.github.com>
2022-04-10 16:44:12 +0530

    Add files via upload

---
+++ extension.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 10 16:32:37 2022
+
+@author: EDITMEDIA
+"""
+
+filename=input("input the file name")
+f_extns=filename.split(".")
+print("the extension of the file is"+(f_extns[-1]))
