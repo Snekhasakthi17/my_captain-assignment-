229bae6087a5ada705d8958667a5d7f6d96a19a7
Snekhasakthi17 <103012502+Snekhasakthi17@users.noreply.github.com>
2022-04-10 16:36:20 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 10 16:16:15 2022
+
+@author: EDITMEDIA
+"""
+
+r=float(input("enter the radius"))
+a=3.14*r*r
+print("radius of the circle:",r)
+print("area of the circle:",a)
