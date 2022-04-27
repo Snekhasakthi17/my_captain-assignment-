84f73ae5e94939478d8c90aa542722906340202f
Snekhasakthi17 <103012502+Snekhasakthi17@users.noreply.github.com>
2022-04-27 19:39:48 +0530

    positive number in range
    this program will execute all the positive number for your given range

---
+++ positive.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 27 19:35:37 2022
+
+@author: EDITMEDIA
+"""
+
+start,end=-4,19
+for num in range(start,end+1):
+    if num>=0:
+        print(num,end=" ")
